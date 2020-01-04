544d214a2bf35fe559be27c8dbe60afb5e24815f
Bennett Bullock <bennett36@gmail.com>
2020-01-04 16:07:36 -0300

    Library to send-receive requests

---
+++ pype3/flask_helpers.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, Response,jsonify
+import requests
+import json
+
+def send_request(url,js):
+
+    s=json.dumps(js)
+    r=requests.post(url,data=s)
+    rJS=json.loads(r.content)
+
+    return rJS

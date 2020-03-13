b636850c91f0e1752f16eb79ec99e701c7ad165b
Bennett Bullock <bennett36@gmail.com>
2020-03-13 16:41:18 -0300

    Including bytes-parsing loader

--- pype3/loaders.py
+++ pype3/loaders.py
@@ -1,5 +1,9 @@
 import json
 import csv
+import os
+import chardet
+import pickle
+
 def dump_json(fileName,js):
 
     with open(fileName,'w') as f:
@@ -21,6 +25,34 @@ def load_json(fileName):
         js=json.loads(f.read())
 
         return js
+
+
+def load_bytes(fileName):
+
+    with open(fileName,'rb') as f:
+
+        text=f.read()
+        encoding=chardet.detect(text)['encoding']
+        text=text.decode(encoding)
+
+        return text
+
+
+def load_pickle(fileName):
+
+    with open(fileName,'rb') as f:
+
+        return pickle.load(f)
+
+
+def os_listdir(dirName):
+
+    return os.listdir(dirName)
+
+
+def os_join(txt1,txt2):
+
+    return os.path.join(txt1,txt2)
 
 
 # def load_csv(fileName,delimiter=','):

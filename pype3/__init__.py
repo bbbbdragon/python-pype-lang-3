047d88da05763cb20dfb69cf4f6d5e36524cc095
Bennett Bullock <bennettbullock36@gmail.com>
2021-02-24 15:47:05 -0300

    Bumping version

--- pype3/__init__.py
+++ pype3/__init__.py
@@ -1,5 +1,5 @@
 name='pype3'
-__version__='3.0.29'
+__version__='3.0.30'
 py_slice=slice
 from pype3.build_helpers import *
 from pype3.nodes import *
@@ -99,7 +99,7 @@ def add_main_modules(mod,glbls):
 
                 modName='numpy'
 
-            else:
+            elif hasattr(attr,'__module__'):
 
                 modName=attr.__module__
 

0fa0d5f0fa6e68b268d2972256776cb5a56e8336
Bennett Bullock <bennettbullock36@gmail.com>
2021-04-14 11:34:13 -0300

    Doing refresh on compilation for notebook runtime

--- pype3/__init__.py
+++ pype3/__init__.py
@@ -1,5 +1,5 @@
 name='pype3'
-__version__='3.0.32'
+__version__='3.0.33'
 py_slice=slice
 from pype3.build_helpers import *
 from pype3.nodes import *
@@ -29,7 +29,7 @@ from pype3.fargs import _,_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_last
 from pype3.macros import ep,iff,ift,ifp,iftp,db,dbp,mp,l,lm,ifa,ifta,ea,dp
 from pype3.macros import select,a,ap,m,d,tup,app,c,is_true,squash,change
 from pype3.macros import cl,dm,cl_if,cl_has,cl_app,ext,consec,consec_dct
-from pype3.macros import embed,td,tdm
+from pype3.macros import embed,td,tdm,ddb
 # from numba import njit
 # import astor
 
@@ -413,6 +413,7 @@ pype_builder=pypeify
 #####################################
 
 def pypeify_namespace(namespace,
+                      recompile=False,
                       functionCache=FUNCTION_CACHE):
     '''
     This function searches a namespace for any pype functions which do not have the
@@ -461,6 +462,12 @@ def pypeify_namespace(namespace,
     aliases=aliases_for_pype(namespace)
     allPypeFunctions={k:v for (k,v) in namespace.items() \
                       if is_pype_function(v,aliases)}
+
+    if recompiled:
+
+        for k in allPypeFunctions:
+
+            del functionCache[k]
 
     for (k,v) in allPypeFunctions.items():
         

3630fef7c33a2949ef671c30fd48c553bc5c0cd2
Bennett Bullock <bennettbullock36@gmail.com>
2021-04-19 18:15:25 -0300

    Bumping version

--- pype3/__init__.py
+++ pype3/__init__.py
@@ -1,5 +1,5 @@
 name='pype3'
-__version__='3.0.37'
+__version__='3.0.38'
 py_slice=slice
 from pype3.build_helpers import *
 from pype3.nodes import *
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 #    LONG_DESCRIPTION=fh.read()
 
 setup(name='python-pype-lang-3',
-      version='3.0.37',
+      version='3.0.38',
       description='Version 3 of pype, a Python-integrated functional programming language - Provides easy-to-use pseudo-macros for common functional programming tasks such as maps, reduces, filters, conditionals, dictionary manipulations, and list manipulations..',
       #long_description_content_type="text/markdown",
       #long_description=LONG_DESCRIPTION,
@@ -26,7 +26,7 @@ setup(name='python-pype-lang-3',
           # 'numba',
           'colorama',
           'astpretty',
-          #'mmh3',
+          'mmh3',
           'requests',
           'flask',
           'twine',

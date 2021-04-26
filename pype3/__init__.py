19c4e0b2741ee64dbff183f81522f51e8da2bc4d
Bennett Bullock <bennettbullock36@gmail.com>
2021-04-26 16:55:50 -0300

    Upgrading version

--- pype3/__init__.py
+++ pype3/__init__.py
@@ -1,5 +1,5 @@
 name='pype3'
-__version__='3.0.38'
+__version__='3.0.39'
 py_slice=slice
 from pype3.build_helpers import *
 from pype3.nodes import *
@@ -29,7 +29,7 @@ from pype3.fargs import _,_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_last
 from pype3.macros import ep,iff,ift,ifp,iftp,db,dbp,mp,l,lm,ifa,ifta,ea,dp
 from pype3.macros import select,a,ap,m,d,tup,app,c,is_true,squash,change
 from pype3.macros import cl,dm,cl_if,cl_has,cl_app,ext,consec,consec_dct
-from pype3.macros import embed,td,tdm,ddb
+from pype3.macros import embed,td,tdm,ddb,ifnot
 # from numba import njit
 # import astor
 
